ec0380af20fec5a1c8ec1f133ba26fb024256302
Nick Yahnke <nick@pitchinteractive.com>
2020-12-30 23:19:51 -0800

    group messages by month

--- index.js
+++ index.js
@@ -4,30 +4,44 @@ const yargs = require('yargs/yargs');
 
 
 const argv = yargs(process.argv.slice(2))
-  .command('inputDirectory', 'specify the path to the exported slack data', {
-    inputDirectory: {
-      description: 'the input directory',
-      alias: 'i',
-      type: 'string',
-      demandOption: true,
-    }
-  }).demandOption(['inputDirectory']).argv
+  .describe('inputDirectory', 'specify the path to the exported slack data')
+  .describe('splitMonths', 'split output by months')
+  .default('splitMonths', false)
+  .demandOption(['inputDirectory']).argv
 
 console.log(argv)
 
 
-const channels = JSON.parse(fs.readFileSync(path.join(argv.inputDirectory, 'channels.json')))
+const { inputDirectory, splitMonths } = argv
+
+const channels = JSON.parse(fs.readFileSync(path.join(inputDirectory, 'channels.json')))
 
 console.log(channels)
 
 channels.forEach(channel => {
   const name = channel.name
-  const channelLogs = fs.readdirSync(path.join(argv.inputDirectory, name))
+  const channelLogs = fs.readdirSync(path.join(inputDirectory, name))
   console.log(channelLogs)
   const channelMessages = []
+  let writeOutData = false
+  const groupedChannelMessages = {}
+  let groupAccessor = date => 'all'
+  if (splitMonths) {
+    groupAccessor = date => date.substr(0, 7)
+  }
+
   channelLogs.forEach(date => {
-    const dateMessages = JSON.parse(fs.readFileSync(path.join(argv.inputDirectory, name, date)))
-    channelMessages.push(...dateMessages)
+    const dateMessages = JSON.parse(fs.readFileSync(path.join(inputDirectory, name, date)))
+    const group = groupAccessor(date)
+    // channelMessages.push(...dateMessages)
+    if (!groupedChannelMessages[group]) {
+      groupedChannelMessages[group] = []
+    }
+    groupedChannelMessages[group].push(...dateMessages)
   })
-  fs.writeFileSync(path.join(argv.inputDirectory, `combined-${name}.json`), JSON.stringify(channelMessages))
+  Object.keys(groupedChannelMessages).forEach(group => {
+    const messages = groupedChannelMessages[group]
+    fs.writeFileSync(path.join(inputDirectory, `combined-${name}-${group}.json`), JSON.stringify(messages))
+
+  })
 })
